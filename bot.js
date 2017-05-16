9d84333c7a7bdcf7b1b0efc36f20dd7bc83494d9
sapblatt11 <sapblatt11@gmail.com>
2017-05-15 21:45:41 -0400

    Fixed Syntax

--- bot.js
+++ bot.js
@@ -1,19 +1,22 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const moment = require('moment')
-  const Time = moment().format('M Do| h:mm:ss | ');
+const moment = require('moment');
+const Time = moment().format('M Do| h:mm:ss | ');
 
 client.login(config.token);
 
-client.on('ready',() =>{
-          console.log (Time + 'Selfbot is Online')
-          });
+client.on('ready', () => {
+  console.log(Time + 'Selfbot is Online');
+});
 client.on('message', message => {
-  if(message.author !== client.user) return;
-    
-    let prefix='.';
-    
-    if (!message.content.startsWith(prefix)) return;
+  if (message.author !== client.user) {
+    return;
+  }
 
+  let prefix = '.';
+
+  if (!message.content.startsWith(prefix)) {
+    return;
+  }
 });
