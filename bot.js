4151fdaeb919b22daa80950b40bdea4ffc0a2b1a
sapblatt11 <sapblatt11@gmail.com>
2017-05-22 21:36:02 -0400

    Added Eval

--- bot.js
+++ bot.js
@@ -1,28 +1,43 @@
+/* eslint-disable no-console, eslint-disable no-eval */
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const moment = require('moment');
 const Time = moment().format('M Do| h:mm:ss | ');
 
+const clean = text => {
+  if (typeof (text) === 'string') {
+    return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+  }
+  return text;
+};
 
-const num = parseInt(message.content.split(' ')[1]);
-const rand = Math.floor(Math.random()*num);
 client.login(config.token);
 
 client.on('ready', () => {
   console.log(Time + 'Selfbot is Online');
+  client.user.setGame('with codez');
 });
-client.on('message', message => {
-  if (message.author !== client.user) {
+client.on('message', message => { 
+  if (message.author.id !== client.user.id) { /*Only my usage*/
     return;
   }
+  const args = message.content.split(' ').slice(1);
 
-  let prefix = '.';
+  if (message.content.startsWith('.eval')) {
+    try {
+      const code = args.join(' ');
+      let evaled = eval(code);
 
-  if (!message.content.startsWith(prefix)) {
-    return;
-    if(message.content === prefix + "rng"){
-      message.content.split(' ')[1] // => "56"
-      client.msg.reply (rand);
+      if (typeof evaled !== 'string') {
+        evaled = require('util').inspect(evaled);
+      }
+
+      message.edit('Input :envelope_with_arrow: :\n' + '```js\n' + code + '```' + '\n\n' + 'Output: :mailbox_with_no_mail: \n' + '```fix\n' + clean(evaled) + '```');
+    } catch (err) {
+      message.edit(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
   }
-  }});
+  let prefix = '.'; // zalways use a prefix it's good practice.
+});
+
