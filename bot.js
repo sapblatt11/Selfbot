5bc39a92618738e115c87fe2012b23f2a8c147f0
sapblatt11 <sapblatt11@gmail.com>
2017-05-28 21:53:26 -0400

    Added Urban Dict Search
    -Added Urban Dict Search
    -Added Statistics
    -Removed Delete
    -Changed Prefix to '>'

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ const Time = moment().format('MMM D| h:mm:ss | ');
 const chalk = require('chalk');
 const lmgtfy = require('lmgtfy');
 
+const urban = require('relevant-urban');
+
 const clean = text => {
   if (typeof (text) === 'string') {
     return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
@@ -19,35 +21,63 @@ const clean = text => {
 client.login(config.token);
 
 client.on('ready', () => {
-  console.log(chalk.cyan(Time + 'Selfbot is Online'));
+  console.log(chalk.cyan(Time + chalk.green('Selfbot is Online')));
 });
 client.on('message', message => {
   if (message.author.id !== client.user.id) { /* Only my usage */
     return;
   }
-  if (message.content === '.cmd') {
+
+  const duration = moment(client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+  if (message.content === '>stat') {
+    message.edit(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime     :: ${duration}
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.size.toLocaleString()}
+• Channels   :: ${client.channels.size.toLocaleString()}
+• Discord.js :: v${Discord.version}`);
+    console.log(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime     :: ${duration}
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.size.toLocaleString()}
+• Channels   :: ${client.channels.size.toLocaleString()}
+• Discord.js :: v${Discord.version}`);
+  }
+
+  if (message.content === '>cmd') {
     if (message.channel.type === 'text') {
       message.delete();
       client.users.get('277544762326777874').send('help');
       console.log(chalk.yellow(Time) + chalk.green('Exec\'d Cmd'));
     }
   }
-  if (message.content === '.tflip') {
+  if (message.content.startsWith('>urban')) {
+    let args = message.content.split(' ').slice(1);
+    let pop = args.join(' ');
+    let op = urban(pop).then(m => {
+      const embed = {
+        title: pop,
+        description: '```css\n' + m.definition + '```',
+        color: 8624466
+      };
+      message.edit({embed});
+      console.log(chalk.yellow(Time) + chalk.green('Found ' + Number(chalk.magenta(args)) + 'in Urban Dict.'));
+    });
+  }
+  if (message.content === '>tflip') {
     message.edit(' (╯°□°）╯︵ ┻━┻');
     console.log(chalk.yellow(Time) + chalk.green('Exec\'d tFlip'));
   }
-  if (message.content === '.delete') {
-    message.delete(500);
-    console.log(chalk.yellow(Time) + chalk.green('Exec\'d delete last'));
-  }
-  if (message.content === '.ping') {
+  if (message.content === '>ping') {
     message.delete();
     message.channel.send('Pinging...').then(sent => {
       sent.edit(`Pong! Took ${sent.createdTimestamp - message.createdTimestamp}ms (${(sent.createdTimestamp - message.createdTimestamp) * 0.001}s)`);
       console.log(chalk.yellow(Time) + chalk.green('Ping\'d'));
     });
   }
-  if (message.content.startsWith('.lmgtfy')) {
+  if (message.content.startsWith('>lmgtfy')) {
     let args = message.content.split(' ').slice(1);
     let pop = args.join(' ');
     let op = lmgtfy(pop);
@@ -58,7 +88,7 @@ client.on('message', message => {
     /* START Eval Command */
   const args = message.content.split(' ').slice(1);
 
-  if (message.content.startsWith('.eval')) {
+  if (message.content.startsWith('>eval')) {
     try {
       const code = args.join(' ');
       let evaled = eval(code);
@@ -75,13 +105,13 @@ client.on('message', message => {
   }  /* END Eval Command */
     /* Text Formatter Stolen From https://github.com/Nyxiis/selfbot/blob/master/index.js */
     /* SpongeBob Mock Below */
-  if (message.content.startsWith('.l ')) { /* SpongeBob Mock */
+  if (message.content.startsWith('>leet ')) { /* SpongeBob Mock */
     upper(message, message.content.split(' ').slice(1).join(' '));
     console.log(chalk.yellow(Time) + chalk.green('Exectued Meme Text'));
   }
     /* Prune Delete */
   const params = message.content.split(' ').slice(1);
-  if (message.content.startsWith('.prune')) {
+  if (message.content.startsWith('>prune')) {
     // get number of messages to prune
     let messagecount = parseInt(params[0]);
     // get the channel logs
@@ -100,6 +130,7 @@ client.on('message', message => {
       });
   }
 });
+
 function upper(message, suffix) {
   suffix = suffix.toLowerCase();
   suffix = suffix.split('');
